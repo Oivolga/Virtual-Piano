5620aefff467cabb436fb3e936d1fcd527a16183
Oivolga <80308919+Oivolga@users.noreply.github.com>
2022-03-19 17:16:59 +0300

    Add files via upload

---
+++ Virtual Piano/task/src/main.js
@@ -0,0 +1,41 @@
+
+document.addEventListener("keydown",function(event){
+    if (event.key.toUpperCase() === "A") {
+        let audio = new Audio("media/A.mp3");
+        audio.play();
+    } else if (event.key.toUpperCase() === "S") {
+        let audio = new Audio("media/S.mp3");
+        audio.play();
+    } else if (event.key.toUpperCase() === "D") {
+        let audio = new Audio("media/D.mp3");
+        audio.play();
+    } else if (event.key.toUpperCase() === "F") {
+        let audio = new Audio("media/F.mp3");
+        audio.play();
+    } else if (event.key.toUpperCase() === "G") {
+        let audio = new Audio("media/G.mp3");
+        audio.play();
+    } else if (event.key.toUpperCase() === "H") {
+        let audio = new Audio("media/H.mp3");
+        audio.play();
+    } else if (event.key.toUpperCase() === "J") {
+        let audio = new Audio("media/J.mp3");
+        audio.play();
+    } else if (event.key.toUpperCase() === "W") {
+        let audio = new Audio("media/W.mp3");
+        audio.play();
+    } else if (event.key.toUpperCase() === "E") {
+        let audio = new Audio("media/E.mp3");
+        audio.play();
+    } else if (event.key.toUpperCase() === "T") {
+        let audio = new Audio("media/T.mp3");
+        audio.play();
+    } else if (event.key.toUpperCase() === "Y") {
+        let audio = new Audio("media/Y.mp3");
+        audio.play();
+    } else if (event.key.toUpperCase() === "U") {
+        let audio = new Audio("media/U.mp3");
+        audio.play();
+    }
+});
+
